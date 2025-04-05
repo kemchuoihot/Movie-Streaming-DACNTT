02885c31b63cdd6dca9b9daab5d2557d114bd47a
AnhThy <lamnguyenanhthy2018@gmail.com>
2025-04-05 13:22:35 +0700

    thêm đường dẫn đến các trang đăng kí đăng nhập

--- client/src/App.js
+++ client/src/App.js
@@ -6,11 +6,17 @@ import Category from './Components/Category/Category';
 import Search from './Components/Search/Search';
 import Detail from './Components/Detail/Detail';
 import Watch from './Components/Watch/Watch';
+import RegisterPage from './Components/Login/Register';
+import LoginPage from './Components/Login/Login';
+import ForgetPasswordPage from './Components/Login/ForgetPassword';
 
 function App() {
   return (
     <Router>
       <Routes>
+        <Route path='/signup' element={<RegisterPage />} />
+        <Route path='/signin' element={<LoginPage />} />
+        <Route path='/forgetpassword' element={<ForgetPasswordPage />} />
         <Route path='/' element={<Layout />} />
         <Route path='/category/:category/:page' element={<Category />} />
         <Route path='/search' element={<Search />} />
